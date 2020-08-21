c33e35600722f3b2f0491efbda20a0def75f204d
trudova <liubovtrudova@gmail.com>
2020-08-21 16:35:34 -0400

    set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Home from './pages';
-import Solutions from './pages/Solutions'
-import Navbar from './components/Navbar'
+import Home from './pages/Home';
+import Solutions from './pages/Solutions';
+import Navbar from './components/Navbar';
+import {Route, Switch} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Home/>
       <Solutions/>
     </div>
