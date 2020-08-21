9f774567bac2b8cb9e6f1e5582e21ba2fc9ea805
trudova <liubovtrudova@gmail.com>
2020-08-21 17:57:19 -0400

    set ups

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
-//import Solutions from './pages/Solutions';
+import Solutions from './pages/Solutions';
 import Navbar from './components/Navbar';
+import SinglSolution from './pages/SinglSolution'
 import Error from './pages/Error';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Router} from 'react-router-dom';
 function App() {
   return (
     <>
       <Navbar/>
-      <Home/>
-      {/* //<Solutions/> */}
-      <Error/>
+      <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/solutions/' component={Solutions}/>
+      <Route exact path='/solutions/:slug' component={SinglSolution}/>
+      <Route component={Error}/>
+      </Switch>
+      
     </>
   );
 }
---
+++ src/pages/SinglSolution.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SinglSolution extends Component {
+    render() {
+        return (
+            <div>
+                hello from singl SinglSolution
+            </div>
+        )
+    }
+}
