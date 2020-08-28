905a5ddc20a887d3b0bb0c01b598806ba050207e
trudova <liubovtrudova@gmail.com>
2020-08-28 18:33:51 -0400

    banner and css

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
 export default function Error() {
     return (
-        <Hero/>
+        <Hero>
+            <Banner title="404" subtitle="Страница не найдена">
+                <Link to='/' className="btn-primary">
+вернуться домой</Link>
+            </Banner>
+        </Hero>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
 export default function Home() {
     return (
-        <Hero />
+        <Hero>
+            <Banner title="BRAIN HOUSE" subtitle=" Наши усилия направлены на поиск, агрегацию и адаптацию передовых Российских и зарубежных высоких технологий к современным технологическим вызовам, требованиям бизнеса в условиях современного Мира.">
+<Link to ='/solutions' className='btn-primary'>наши решения</Link>
+            </Banner>
+        </Hero>
     )
 }
