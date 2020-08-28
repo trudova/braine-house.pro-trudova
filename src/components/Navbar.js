1b8621bf7708b914c0f927782fab1054dc54fa59
trudova <liubovtrudova@gmail.com>
2020-08-28 14:17:47 -0400

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import logo from '../img/brain house03_02.tif';
 import {FaAlignRight} from 'react-icons/fa';
-import{Linl} from 'react-router-dom'
+import{Link} from 'react-router-dom'
 export default class Navbar extends Component {
     render() {
         return (
