b2bed8d87d23acccc9d946c304a13c6a8eab00d4
trudova <liubovtrudova@gmail.com>
2020-08-28 19:21:28 -0400

    solutions banner

--- src/pages/Solutions.js
+++ src/pages/Solutions.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
 export default function Solutions() {
     return (
-        <Hero hero="solutionsHero"/>
+        <Hero hero="solutionsHero">
+            <Banner title="IT и телекоммуникация
+" subtitle="Наша компания является открытой бизнес средой и технологической площадкой для сотрудничества в области инноваций и совместной реализации проектов."><Link to='/' className="btn-primary">
+вернуться домой</Link></Banner>
+        </Hero>
     )
 }
