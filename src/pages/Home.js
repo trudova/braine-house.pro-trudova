c90f33d2c334f60a939ff6e1101dcebbf804812f
trudova <liubovtrudova@gmail.com>
2020-08-28 23:35:37 -0400

    BH info

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import {Link} from 'react-router-dom'
+import BH from '../components/BH'
 export default function Home() {
     return (
+        <>
         <Hero>
             <Banner title="BRAIN HOUSE" subtitle=" Наши усилия направлены на поиск, агрегацию и адаптацию передовых Российских и зарубежных высоких технологий к современным технологическим вызовам, требованиям бизнеса в условиях современного Мира.">
 <Link to ='/solutions' className='btn-primary'>наши решения</Link>
             </Banner>
         </Hero>
+        <BH/>
+        </>
     )
 }
